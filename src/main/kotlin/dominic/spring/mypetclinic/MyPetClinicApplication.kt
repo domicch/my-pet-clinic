759a0fcd345596fd2c8772213cf2b604326b39b4
domicch <domicch@gmail.com>
2021-03-11 01:17:46 +0000

    Initial project after Spring Initializr

---
+++ src/main/kotlin/dominic/spring/mypetclinic/MyPetClinicApplication.kt
@@ -0,0 +1,11 @@
+package dominic.spring.mypetclinic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyPetClinicApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyPetClinicApplication>(*args)
+}
